217271dbfab84f651f4aba31f29cff0d0ef570ab
Syed Osama Ahmed <batch17.026@gmail.com>
2022-11-25 16:02:08 +0500

    learnt http methods

--- main.js
+++ main.js
@@ -37,6 +37,10 @@ app.post("/api/people",(req,res)=>{
 })
 
 
+
+
+
+
 app.put("/api/people/:id",(req,res)=>{
   const {name} =req.body;
   const {id} =req.params;
@@ -59,6 +63,22 @@ app.put("/api/people/:id",(req,res)=>{
   success:true,
   data:newPeople
  })
+})
+
+app.delete("/api/people/:id",(req,res)=>{
+  const person=people.find(person=>person.id===Number(req.params.id));
+  if(!person){
+    return res.status(404).json({
+      success:false,
+      msg:`No person with id:${req.params.id}`
+    })
+  }
+
+  const newPeople=people.filter(person=>person.id!==Number(req.params.id))
+  return res.status(200).json({
+    success:true,
+    data:newPeople
+  })
 })
 app.listen(5000, () => {
   console.log("Server running at port 5000");
